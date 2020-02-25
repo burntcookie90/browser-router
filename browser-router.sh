d847a3f73171178d5619ed044fbe22a0074c39bb
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2020-02-25 10:01:48 -0500

    Update browser-router.sh

--- browser-router.sh
+++ browser-router.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 url=$1
-check=$(echo "$url" | grep -qFf /home/vishnu/repos/shell/browser-router/url-whitelist.txt)
+check=$(echo "$url" | grep -qFf {path-to}/url-whitelist.txt)
 if [ $? -eq 0 ];then
   google-chrome $url
 else
